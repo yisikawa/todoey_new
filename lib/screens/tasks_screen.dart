a6463968dfc91284a164e701dc98bafa6e5f4a7c
Yoshiaki Ishikawa <yisikawa@ma.medias.ne.jp>
2020-04-03 15:21:28 +0900

    i have made skeleton

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,13 +5,59 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Icon(Icons.list),
-            Text('Todoey'),
-          ],
-        ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 60.0,left: 30.0,right: 30.0,bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                    child: Icon(
+                        Icons.list,
+                    size: 30.0,
+                        color: Colors.lightBlueAccent,
+                    ),
+                  backgroundColor: Colors.white,
+                    radius: 30.0,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text('Todoey',
+                style: TextStyle(
+                  fontSize: 50.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                ),),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
