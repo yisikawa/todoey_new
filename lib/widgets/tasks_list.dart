6722037c085b9fa761df26d2f4dd8711389b378b
Yoshiaki Ishikawa <yisikawa@ma.medias.ne.jp>
2020-04-09 15:31:08 +0900

    implement checkbox

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'tasks_tile.dart';
+import 'package:todoeynew/models/task.dart';
 
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name:'Buy milk'),
+    Task(name:'Buy eggs'),
+    Task(name:'Buy bread'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return ListView(
--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('this is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (newValue) {
+      title: Text('this is a task',
+      style: TextStyle(
+        decoration: isChecked? TextDecoration.lineThrough:null,
+      ),),
+      trailing: TaskCheckbox(
+      checkboxState: isChecked,
+      toggleCheckboxCallback: (bool checkboxState) {
+        setState(() {
+          isChecked = checkboxState;
+        });
+      },),
+    );
+  }
+}
 
-        },
-      ),
+class TaskCheckbox extends StatelessWidget {
+  TaskCheckbox({this.checkboxState,this.toggleCheckboxCallback});
+
+  final bool checkboxState ;
+  final Function toggleCheckboxCallback;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxCallback,
     );
   }
 }
